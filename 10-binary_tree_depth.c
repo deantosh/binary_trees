d4813102e8e10061fad117ef17fcb705d7e96429
deantosh <deantoshdaiddoh@gmail.com>
2024-06-05 09:57:10 -0700

    function: binary tree depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth_counter = 0;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t depth_counter = 0;
 
 	while (tree->parent)
 	{
