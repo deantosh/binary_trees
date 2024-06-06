67cd9ccd85183cb743953ec6f90cf69209ad91ee
deantosh <deantoshdaiddoh@gmail.com>
2024-06-06 03:08:40 -0700

    function prototype added

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,8 @@ int tree_height(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
 
 /* HELPER FUNCTION*/
 int full_check_height(const binary_tree_t *tree, int depth, int *leaf_level);
