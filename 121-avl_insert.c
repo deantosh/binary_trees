53d83bf186e48c47331229d984bbc5fcee18fec3
deantosh <deantoshdaiddoh@gmail.com>
2024-06-12 13:55:14 -0700

    file to update

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -9,63 +9,37 @@
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *curr, *new_node, *leaf_node, *unbalanced_node;
-	int balance_factor;
+	avl_t *new_node;
 
-	/*set the current pointer position*/
-	curr = (*tree);
-	/*If tree is empty - create the new node*/
-	if (!curr)
+	/*If tree is empty - add new_node, make it root of tree*/
+	if (*tree == NULL)
 	{
-		new_node = binary_tree_node(curr, value);
+		new_node = binary_tree_node(NULL, value);
 		if (!new_node)
 			return (NULL);
 		*tree = new_node;  /*move head to new node*/
 		return (new_node);
 	}
-	/*Find the leaf_node*/
-	leaf_node = find_leaf_node(curr, value);
-	if (!leaf_node)
-		return (NULL);
-	/*create new node*/
-	new_node = binary_tree_node(leaf_node, value);
-	if (!new_node)
-		return (NULL);
-	/*select which child is the new node- left or right*/
-	if (value < leaf_node->n)
-		leaf_node->left = new_node;
-	if (value > leaf_node->n)
-		leaf_node->right = new_node;
-	/*Handle AVL tree not balance*/
-	unbalanced_node = find_unbalanced_node(curr);
-	balance_factor = binary_tree_balance(unbalanced_node);
-	/*If: +2 - rotate right to balance*/
-	if (balance_factor == 2)
-	{
-		if (binary_tree_balance(unbalanced_node->left) == -1)
-			binary_tree_rotate_left(unbalanced_node->left);
-		binary_tree_rotate_right(unbalanced_node);
-	}
-	/*If: -2 - rotate left to balance*/
-	if (balance_factor == -2)
-	{
-		if (binary_tree_balance(unbalanced_node->right) == 1)
-			binary_tree_rotate_right(unbalanced_node->right);
-		binary_tree_rotate_left(unbalanced_node);
-	}
+	/*If tree not empty: add new_node*/
+	new_node = insert_new_node(*tree, value);
+
+	/*If tree unbalanced: balance it*/
+	balance_tree(tree, new_node);
+
 	return (new_node);
 }
 
 /**
- * find_leaf_node - Function finds the leaf node to add the
- *	new node at its end.
- * @curr: A pointer to the root node to the tree to insert.
+ * insert_new_node - Function inserts a new node in the AVL tree
+ * @curr: A pointer to the current node of the tree.
  * @value: The value of the node to insert to tree.
  *
- * Return: leaf node (success) or NULL (fails).
+ * Return: new_node (success) or NULL (fails).
  */
-avl_t *find_leaf_node(avl_t *curr, int value)
+avl_t *insert_new_node(avl_t *curr, int value)
 {
+	avl_t *new_node;
+
 	/*Base case: terminates the loop*/
 	if (!curr)
 		return (NULL);
@@ -74,47 +48,72 @@ avl_t *find_leaf_node(avl_t *curr, int value)
 	if (value < curr->n)
 	{
 		if (curr->left)
-			return (find_leaf_node(curr->left, value));
+		{
+			return (insert_new_node(curr->left, value));
+		}
 		else
-			return (curr);
+		{
+			/*create the new node*/
+			new_node = binary_tree_node(curr, value);
+			if (!new_node)
+				return (NULL);
+			curr->left = new_node;
+			return (new_node);
+		}
 	}
 	else if (value > curr->n)
 	{
 		if (curr->right)
-			return (find_leaf_node(curr->right, value));
+		{
+			return insert_new_node(curr->right, value);
+		}
 		else
-			return (curr);
+		{
+			/*create the new node*/
+			new_node = binary_tree_node(curr, value);
+			if (!new_node)
+				return (NULL);
+			curr->right = new_node;
+			return (new_node);
+		}
 	}
 
 	return (NULL);
 }
 
 /**
- * find_unbalanced_node - Function finds the node that is unbalanced.
- * @tree: A pointer to the root node of the tree to search.
+ * balance_tree - Function balances the AVL tree after inserting a new node.
+ * @tree: A double pointer to the AVL tree.
+ * @curr: A pointer to the inserted node.
  *
- * Return: unbalanced node (success) or NULL (fails).
+ * Return: void.
  */
-avl_t *find_unbalanced_node(avl_t *tree)
+void balance_tree(avl_t **tree, avl_t *curr)
 {
-	avl_t *left_unbalanced, *right_unbalanced;
+	avl_t *parent;
 	int balance_factor;
 
-	if (!tree)
-		return (NULL);
+	while (curr)
+	{
+		balance_factor = binary_tree_balance(curr);
 
-	/*If node is unbalanced*/
-	balance_factor = binary_tree_balance(tree);
-	if (balance_factor == 2  || balance_factor == -2)
-		return (tree);
+		if (balance_factor > 1)
+		{
+			if (binary_tree_balance(curr->left) < 0)
+				curr->left = binary_tree_rotate_left(curr->left);
+			curr = binary_tree_rotate_right(curr);
+		}
+		else if (balance_factor < -1)
+		{
+			if (binary_tree_balance(curr->right) > 0)
+				curr->right = binary_tree_rotate_right(curr->right);
+			curr = binary_tree_rotate_left(curr);
+		}
 
-	/*Traverse the right and left subtree*/
-	right_unbalanced = find_unbalanced_node(tree->right);
-	if (right_unbalanced)
-		return (right_unbalanced);
-	left_unbalanced = find_unbalanced_node(tree->left);
-	if (left_unbalanced)
-		return (left_unbalanced);
-
-	return (NULL);
+		parent = curr->parent;
+		if (!parent)
+			*tree = curr;  /*Handle new root*/
+		curr = parent;
+	}
 }
+
