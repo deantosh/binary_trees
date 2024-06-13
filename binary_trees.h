85a07d45218293256c45be0e76e98c3528c5e47a
deantosh <deantoshdaiddoh@gmail.com>
2024-06-13 05:24:49 -0700

    function prototype added

--- binary_trees.h
+++ binary_trees.h
@@ -60,6 +60,7 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
 
 /* HELPER FUNCTION*/
 int full_check_height(const binary_tree_t *tree, int depth, int *leaf_level);
